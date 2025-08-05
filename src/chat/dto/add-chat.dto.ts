a10a2c57041a9ce611fde0d9ca87fd43a1dd5563
tibex88 <tibesolomon7@gmail.com>
2025-08-05 06:47:31 +0300

    feat: group chat

---
+++ src/chat/dto/add-chat.dto.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsArray,
+  IsNotEmpty,
+  IsUUID
+} from 'class-validator';
+
+export class AddToChatDto {
+
+  @ApiProperty({
+    description: 'Array of client UUIDs for group sessions',
+    example: [
+      'a7e6d68a-3931-4b90-b9d3-bbd3d40813b1',
+      'c2633123-1e65-4c3e-a2a3-3b8b78901c3f',
+    ],
+  })
+  @IsNotEmpty()
+  @IsArray()
+  @IsUUID('all', { each: true })
+  groupClients?: string[];
+
+}
--- src/chat/dto/create-chat.dto.ts
+++ src/chat/dto/create-chat.dto.ts
@@ -1,17 +1,30 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsUUID } from 'class-validator';
+import { IsArray, IsNotEmpty, IsUUID } from 'class-validator';
+import { RequiredIfPropertyMissing } from 'src/common/validators/required-if.validator';
 
 export class CreateChatDto {
 
   @ApiProperty({
-    example: '1',
+    description: 'Array of client UUIDs for group sessions',
+    example: [
+      'a7e6d68a-3931-4b90-b9d3-bbd3d40813b1',
+      'c2633123-1e65-4c3e-a2a3-3b8b78901c3f',
+    ],
   })
+  @RequiredIfPropertyMissing('client')
+  @IsArray()
+  @IsUUID('all', { each: true })
+  groupClients?: string[];
+
+  @ApiProperty({
+    example: 'a7e6d68a-3931-4b90-b9d3-bbd3d40813b1',
+  })
+  @RequiredIfPropertyMissing('groupClients')
   @IsUUID()
-  @IsNotEmpty()
   client: string;
 
   @ApiProperty({    
-    example: '1',  
+    example: 'c2633123-1e65-4c3e-a2a3-3b8b78901c3f',  
     })
   @IsUUID()
   @IsNotEmpty()
