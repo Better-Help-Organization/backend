35ecc0b576114e05a3864378a5f9ac84f1e9ddc9
tibex88 <tibesolomon7@gmail.com>
2025-08-23 20:12:54 +0000

    live changes

--- src/therapist/dto/update-me.dto.ts
+++ src/therapist/dto/update-me.dto.ts
@@ -1,3 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { IsArray, IsOptional, IsString, ValidateNested } from "class-validator";
+import { CreateIndividualAvailabilityDto } from "src/availability/dto/create-individual-availability.dto";
 import { UpdateUserDto } from "src/common/dto/update-user.dto";
 
-export class UpdateMeDto extends UpdateUserDto {}
+export class UpdateMeDto extends UpdateUserDto {
+    @ApiProperty({
+        example: "A brief bio about the user",
+        description: "Bio of the user",
+        required: false,
+    })
+    @IsOptional()
+    @IsString()
+    bio: string;
+
+    @ApiProperty({ type: [CreateIndividualAvailabilityDto] })
+    @IsArray()
+    @ValidateNested({ each: true })
+    @Type(() => CreateIndividualAvailabilityDto)
+    availability: CreateIndividualAvailabilityDto[];
+    
+}
