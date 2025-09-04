64b6af5a23b088affb1341b1392c3e255ef4176b
tibex88 <tibesolomon7@gmail.com>
2025-09-04 14:46:47 +0000

    feat: add me/matches and fix for circular imports

--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatModule } from 'src/chat/chat.module';
 import { Client } from 'src/common/entities/client.entity';
+import { MatchModule } from 'src/match/match.module';
 import { ModalModule } from 'src/modal/modal.module';
 import { MoodModule } from 'src/mood/mood.module';
 import { SessionModule } from 'src/session/session.module';
@@ -14,8 +15,11 @@ import { ClientService } from './client.service';
     ChatModule,
     SessionModule,
     ModalModule,
-    // forwardRef(()=>MoodModule)
-    MoodModule
+    ModalModule,
+    forwardRef(() => ChatModule),
+    forwardRef(() => SessionModule),
+    forwardRef(() => MoodModule),
+    forwardRef(() => MatchModule),
   ],  
   controllers: [ClientController],
   providers: [ClientService],
--- src/match/match.module.ts
+++ src/match/match.module.ts
@@ -1,20 +1,20 @@
-import { Module } from '@nestjs/common';
-import { MatchService } from './match.service';
-import { MatchController } from './match.controller';
-import { LoggerModule } from 'src/logger/logger.module';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Match } from 'src/common/entities/match.entity';
-import { MatchTherapist } from 'src/common/entities/match-therapist.entity';
-import { TherapistModule } from 'src/therapist/therapist.module';
-import { FirebaseModule } from 'src/firebase/firebase.module';
-import { Preference } from 'src/common/entities/preference.entity';
-import { Answer } from 'src/common/entities/answer.entity';
 import { ClientModule } from 'src/client/client.module';
+import { Answer } from 'src/common/entities/answer.entity';
+import { MatchTherapist } from 'src/common/entities/match-therapist.entity';
+import { Match } from 'src/common/entities/match.entity';
+import { Preference } from 'src/common/entities/preference.entity';
+import { FirebaseModule } from 'src/firebase/firebase.module';
+import { LoggerModule } from 'src/logger/logger.module';
+import { TherapistModule } from 'src/therapist/therapist.module';
+import { MatchController } from './match.controller';
+import { MatchService } from './match.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Match, MatchTherapist, Preference, Answer ]),
-    ClientModule,
+    forwardRef(() => ClientModule),
     TherapistModule,
     LoggerModule,
     FirebaseModule
