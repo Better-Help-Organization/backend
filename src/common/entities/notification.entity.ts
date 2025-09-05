e14bcf2eb76fe91461c1c1f5c1f2085578807d2f
tibex88 <tibesolomon7@gmail.com>
2025-09-05 22:13:00 +0300

    feat: saving notifications

---
+++ src/common/entities/notification.entity.ts
@@ -0,0 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, CreateDateColumn, Entity, ManyToOne } from 'typeorm';
+import { Client } from './client.entity';
+import { CommonEntity } from './common.entity';
+import { Therapist } from './therapist.entity';
+
+@Entity()
+export class Notification extends CommonEntity {
+
+  @Column({ type: 'text', nullable: true })
+  title: string;
+
+  @Column({ type: 'text', nullable: true })
+  body: string;
+
+  @Column({ type: 'text', nullable: true })
+  message: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  code: string;
+
+  @ApiProperty({ type: () => Client })
+  @ManyToOne(() => Client, { nullable: true })
+  client?: Client;
+
+  @ApiProperty({ type: () => Therapist })
+  @ManyToOne(() => Therapist, { nullable: true })
+  therapist?: Therapist;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
---
+++ src/firebase/dto/save-notification.dto.ts
@@ -0,0 +1,10 @@
+
+
+export class SaveNotificationDto {
+  title: string;
+  body: string;
+  message: string;
+  code: string;
+  clientId?: string;
+  therapistId?: string;
+}
