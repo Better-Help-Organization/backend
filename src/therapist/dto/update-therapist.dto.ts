64144baa13d0e02ff909bf16369dc7feed033552
tibex88 <tibesolomon7@gmail.com>
2025-09-06 09:38:34 +0300

    feat: update bio

--- src/therapist/dto/update-therapist.dto.ts
+++ src/therapist/dto/update-therapist.dto.ts
@@ -1,3 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
 import { UpdateUserDto } from 'src/common/dto/update-user.dto';
 
-export class UpdateTherapistDto extends UpdateUserDto {}
+export class UpdateTherapistDto extends UpdateUserDto {
+
+    @ApiProperty({
+        example: "Experienced therapist specializing in cognitive behavioral therapy.",
+        description: "Short biography of the therapist",
+        required: false,
+    })
+    @IsOptional()
+    @IsString()
+    bio: string;
+
+}
