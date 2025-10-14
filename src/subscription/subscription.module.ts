b5f60b64b8b7cafd18550950aec7827090582778
tibex88 <tibesolomon7@gmail.com>
2025-10-14 09:41:52 +0000

    fear: notification on update subscription

--- src/subscription/subscription.module.ts
+++ src/subscription/subscription.module.ts
@@ -4,6 +4,7 @@ import { ClientSubscription } from 'src/common/entities/client-subscription.enti
 import { Client } from 'src/common/entities/client.entity';
 import { Level } from 'src/common/entities/level.entity';
 import { Subscription } from 'src/common/entities/subscription.entity';
+import { FirebaseModule } from 'src/firebase/firebase.module';
 import { LoggerModule } from 'src/logger/logger.module';
 import { SubscriptionController } from './subscription.controller';
 import { SubscriptionService } from './subscription.service';
@@ -12,6 +13,7 @@ import { SubscriptionService } from './subscription.service';
   imports: [
     TypeOrmModule.forFeature([Client, Level, Subscription, ClientSubscription]),
     LoggerModule,
+    FirebaseModule
   ],
   controllers: [SubscriptionController],
   providers: [SubscriptionService],
