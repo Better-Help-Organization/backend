fda9439cb103e4f343b6f130f55379f920e55292
tibex88 <tibesolomon7@gmail.com>
2025-10-18 13:15:24 +0000

    fix: others

--- src/subscription/subscription.module.ts
+++ src/subscription/subscription.module.ts
@@ -3,20 +3,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClientSubscription } from 'src/common/entities/client-subscription.entity';
 import { Client } from 'src/common/entities/client.entity';
 import { Level } from 'src/common/entities/level.entity';
+import { Notification } from 'src/common/entities/notification.entity';
 import { Subscription } from 'src/common/entities/subscription.entity';
+import { Therapist } from 'src/common/entities/therapist.entity';
 import { FirebaseModule } from 'src/firebase/firebase.module';
+import { FirebaseService } from 'src/firebase/firebase.service';
 import { LoggerModule } from 'src/logger/logger.module';
 import { SubscriptionController } from './subscription.controller';
 import { SubscriptionService } from './subscription.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Client, Level, Subscription, ClientSubscription]),
+    TypeOrmModule.forFeature([Client, Level, Subscription, ClientSubscription, Notification, Therapist]),
     LoggerModule,
     FirebaseModule
   ],
   controllers: [SubscriptionController],
-  providers: [SubscriptionService],
+  providers: [SubscriptionService, FirebaseService],
   exports: [SubscriptionService]
 })
 export class SubscriptionModule {}
