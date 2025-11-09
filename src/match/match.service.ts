5fe7ac147afd3b871c84400c3c2273639c492a42
tibex88 <tibesolomon7@gmail.com>
2025-11-09 09:37:31 +0000

    debug: switch back to send to all therapist

--- src/match/match.service.ts
+++ src/match/match.service.ts
@@ -93,17 +93,12 @@ export class MatchService {
       .map(t => t.id)
       .filter(id => id);
 
-    const therapists = await this.therapistService.findMatchingTherapists({
-      gender: preference.gender,
-      level: preference.level?.id,
-      modal: preference.modal?.id,
-      // status: BaseStatus.ACTIVE,
-      // availability: preference.availability.map(a => ({
-      //   day: a.day,
-      //   day_period: a.day_period,
-      // })),
-    });
-    // const {data:therapists} = await this.therapistService.findAll({take:'0'});
+    // const therapists = await this.therapistService.findMatchingTherapists({
+    //   gender: preference.gender,
+    //   level: preference.level?.id,
+    //   modal: preference.modal?.id,
+    // });
+    const {data:therapists} = await this.therapistService.findAll({take:'0'});
 
     if (therapists?.length === 0) {
       throw new NotFoundException('No therapists match your preferences');
