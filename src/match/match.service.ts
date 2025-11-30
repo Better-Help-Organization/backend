e67b927beca0e445f905b532ef9c72dbe6634639
tibex88 <tibesolomon7@gmail.com>
2025-11-30 07:51:47 +0000

    chore: switch to modal specific matching

--- src/match/match.service.ts
+++ src/match/match.service.ts
@@ -94,12 +94,12 @@ export class MatchService {
       .map(t => t.id)
       .filter(id => id);
 
-    // const therapists = await this.therapistService.findMatchingTherapists({
-    //   gender: preference.gender,
-    //   level: preference.level?.id,
-    //   modal: preference.modal?.id,
-    // });
-    const {data:therapists} = await this.therapistService.findAll({take:'0'});
+    const therapists = await this.therapistService.findMatchingTherapists({
+      gender: preference.gender,
+      level: preference.level?.id,
+      modal: preference.modal?.id,
+    });
+    // const {data:therapists} = await this.therapistService.findAll({take:'0'});
 
     if (therapists?.length === 0) {
       throw new NotFoundException('No therapists match your preferences');
