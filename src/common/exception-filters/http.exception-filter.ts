44192a88a03a78a12e7da423619d6f8400d1c85f
tibex88 <tibesolomon7@gmail.com>
2025-12-09 09:05:56 +0000

    chore: modify exception handler

--- src/common/exception-filters/http.exception-filter.ts
+++ src/common/exception-filters/http.exception-filter.ts
@@ -1,6 +1,17 @@
-import { ArgumentsHost, Catch, ExceptionFilter, ExecutionContext, HttpException, Logger } from "@nestjs/common";
+import { ArgumentsHost, Catch, ExceptionFilter, ExecutionContext, HttpException } from "@nestjs/common";
 import { Request, Response } from "express";
 import { LoggerService } from "src/logger/logger.service";
+
+
+interface StandardErrorResponse {
+  message: string | string[];
+  timestamp: string;
+  path: string;
+  statusCode: number;
+  // items?: string[];               // optional, for conflicts
+  // link?: string;                  // optional, for reference
+  // [key: string]: any;             // allow extra fields dynamically
+}
 
 
 @Catch(HttpException)
@@ -14,19 +25,20 @@ export class HttpExceptionFilter implements ExceptionFilter {
         const response = ctx.getResponse<Response>();
         const request = ctx.getRequest<Request>();
         const status = exception.getStatus();
-        let errorDetails: Record<string, any>;
+        let errorDetails: StandardErrorResponse = {
+            message: '',
+            timestamp: new Date().toISOString(),
+            path: request.url,
+            statusCode: status,
+          };
 
-        if (typeof exception.getResponse() === 'string') {
-            // Handle string error messages
-            errorDetails = { message: exception.getResponse() };
-          } else {
-            // Handle object error messages
-            errorDetails = exception.getResponse() as Record<string, any>;
-          }
-          
-        // const errorDetails = exception.getResponse();
-        errorDetails['timestamp'] = new Date().toISOString(),
-        errorDetails['path'] = request.url,
+        const exceptionResponse = exception.getResponse();
+
+        if (typeof exceptionResponse === 'string') {
+          errorDetails.message = exceptionResponse;
+        } else if (typeof exceptionResponse === 'object') {
+          errorDetails = { ...errorDetails, ...exceptionResponse };
+        }
 
         this.logger.captureRequestContext(host as ExecutionContext, HttpExceptionFilter.name)
         this.logger.error(
