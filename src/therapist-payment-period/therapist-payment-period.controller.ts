9c60eee10b0785dacf8fc9f4125d63b80cbed835
tibex88 <tibesolomon7@gmail.com>
2025-12-25 09:53:41 +0000

    feat: endpoint for weekly revenue

--- src/therapist-payment-period/therapist-payment-period.controller.ts
+++ src/therapist-payment-period/therapist-payment-period.controller.ts
@@ -1,15 +1,46 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { ApiQuery } from '@nestjs/swagger';
+import { TokenPayload, UserTypes } from 'src/common/constants';
+import { AllowAdminAccess } from 'src/common/decorators/allow-admin-acess';
 import { DynamicGuards } from 'src/common/decorators/dynamic-guard.decorator';
 import { AdminJwtAuthGuard, TherapistJwtAuthGuard } from 'src/common/guard/jwt-auth.guard';
-import { ApiFindAllQueryParams, ApiFindOneQueryParams, FindAllQueryParams } from 'src/common/middlewares/api-features.dto';
+import { ApiFilterByDate, ApiFindAllQueryParams, ApiFindOneQueryParams, FindAllQueryParams } from 'src/common/middlewares/api-features.dto';
+import { TherapistStatisticsService } from 'src/therapist/therapist.stats';
 import { CreateTherapistPaymentPeriodDto } from './dto/create-therapist-payment-period.dto';
 import { UpdateTherapistPaymentPeriodDto } from './dto/update-therapist-payment-period.dto';
 import { TherapistPaymentPeriodService } from './therapist-payment-period.service';
 
 @Controller('therapist-payment-period')
 export class TherapistPaymentPeriodController {
-  constructor(private readonly therapistPaymentPeriodService: TherapistPaymentPeriodService) {}
+  constructor(
+    private readonly therapistPaymentPeriodService: TherapistPaymentPeriodService,
+    private readonly stats: TherapistStatisticsService,
+  ) {}
 
+    
+  @Get('rot')
+  @ApiFilterByDate()
+  @ApiQuery({ 
+    name: 'mockId', 
+    required: false, 
+    type: String, 
+  })
+  @DynamicGuards(
+    new AdminJwtAuthGuard(),
+    new TherapistJwtAuthGuard()
+  )
+  rot(
+    @AllowAdminAccess(UserTypes.THERAPIST) therapist: TokenPayload,
+    @Query('startDate') startDate?: string,
+    @Query('endDate') endDate?: string,
+    @Query('mockId') mockId?: string
+  ) {
+    if (mockId) {
+      therapist = { id: mockId } as TokenPayload;
+    }
+    return this.stats.getRevenueOverTime(startDate, endDate, therapist.id);
+  }
+  
   @Post()
   @DynamicGuards(
     new AdminJwtAuthGuard(),
