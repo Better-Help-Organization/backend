8b1cd2889faec4c99077d1160eb570f5cb647a54
tibex88 <tibesolomon7@gmail.com>
2026-08-11 15:59:05 +0300

    fix: config only loads env files on firebase

--- src/firebase/firebase.module.ts
+++ src/firebase/firebase.module.ts
@@ -1,21 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as apn from 'apn'; // Make sure to run: npm install apn
-import * as dotenv from 'dotenv';
 import * as admin from 'firebase-admin';
 import * as path from 'path';
 import { Client } from 'src/common/entities/client.entity';
 import { Notification } from 'src/common/entities/notification.entity';
 import { Therapist } from 'src/common/entities/therapist.entity';
 import { FirebaseService } from './firebase.service';
-
-dotenv.config();
-if (!process.env.FIREBASE_SERVICE_ACCOUNT_PATH) {
-  throw new Error('FIREBASE_SERVICE_ACCOUNT_PATH env variable is not set');
-}
-
-const serviceAccount = path.resolve(__dirname, process.env.FIREBASE_SERVICE_ACCOUNT_PATH);
 
 @Module({
   imports:[
@@ -24,10 +17,16 @@ const serviceAccount = path.resolve(__dirname, process.env.FIREBASE_SERVICE_ACCO
   providers: [
     {
       provide: 'FIREBASE_ADMIN',
-      useFactory: () => {
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const serviceAccountPath = configService.get<string>('FIREBASE_SERVICE_ACCOUNT_PATH');
+        if (!serviceAccountPath) {
+          throw new Error('FIREBASE_SERVICE_ACCOUNT_PATH env variable is not set');
+        }
+
         if (!admin.apps.length) {
           admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount),
+            credential: admin.credential.cert(path.resolve(__dirname, serviceAccountPath)),
           });
         }
         return admin;
@@ -35,15 +34,20 @@ const serviceAccount = path.resolve(__dirname, process.env.FIREBASE_SERVICE_ACCO
     },
     {
       provide: 'APN_PROVIDER',
-      useFactory: () => {
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
         // Only initialize if keys are present to prevent crashes in dev
-        if (process.env.APN_KEY_PATH && process.env.APN_KEY_ID && process.env.APN_TEAM_ID) {
+        const apnKeyPath = configService.get<string>('APN_KEY_PATH');
+        const apnKeyId = configService.get<string>('APN_KEY_ID');
+        const apnTeamId = configService.get<string>('APN_TEAM_ID');
+
+        if (apnKeyPath && apnKeyId && apnTeamId) {
           try {
             return new apn.Provider({
               token: {
-                key: path.resolve(__dirname, process.env.APN_KEY_PATH),
-                keyId: process.env.APN_KEY_ID,
-                teamId: process.env.APN_TEAM_ID,
+                key: path.resolve(__dirname, apnKeyPath),
+                keyId: apnKeyId,
+                teamId: apnTeamId,
               },
               production: true,
             });
@@ -57,13 +61,18 @@ const serviceAccount = path.resolve(__dirname, process.env.FIREBASE_SERVICE_ACCO
     },
     {
       provide: 'APN_PROVIDER_SECOND',
-      useFactory: () => {
-        if (process.env.APN2_KEY_PATH) {
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const apn2KeyPath = configService.get<string>('APN2_KEY_PATH');
+        const apn2KeyId = configService.get<string>('APN2_KEY_ID');
+        const apnTeamId = configService.get<string>('APN_TEAM_ID');
+
+        if (apn2KeyPath && apn2KeyId && apnTeamId) {
           return new apn.Provider({
             token: {
-              key: path.resolve(__dirname, process.env.APN2_KEY_PATH),
-              keyId: process.env.APN2_KEY_ID,
-              teamId: process.env.APN_TEAM_ID,
+              key: path.resolve(__dirname, apn2KeyPath),
+              keyId: apn2KeyId,
+              teamId: apnTeamId,
             },
             production: true,
           });
